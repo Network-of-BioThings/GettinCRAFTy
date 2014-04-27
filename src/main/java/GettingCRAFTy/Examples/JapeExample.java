d8d7ac84101e9a1c8cb7c593edeb5c53f9f86268
Kevin Livingston <livingston@dn51dri0.sunet>
2014-04-27 13:01:07 -0600

    added CL ontology from CRAFT, and exaple printing the classes and labels.

--- src/main/java/GettingCRAFTy/Examples/JapeExample.java
+++ src/main/java/GettingCRAFTy/Examples/JapeExample.java
@@ -9,6 +9,7 @@ import gate.creole.*;
 import gate.util.*;
 import gate.util.persistence.PersistenceManager;
 import gate.corpora.RepositioningInfo;
+import gate.creole.ontology.*;
 
 /**
  * This class illustrates how to use ANNIE as a sausage machine
@@ -50,7 +51,56 @@ public class JapeExample  {
 						new File(japeFilePath).toURI().toURL(),
       						"encoding", "UTF-8")); 
 
+      // // Find the directory for the Ontology plugin 
+      // 2File pluginHome = 
+      // 3 new File(new File(Gate.getGateHome(), "plugins"), "Ontology"); 
+      // 4// Load the plugin from that directory 
+      // 5Gate.getCreoleRegister().registerDirectories(pluginHome.toURI().toURL());
+
+
+      // step 1: initialize GATE 
+      //if(!Gate.isInitialized()) { Gate.init(); } 
+      
+      // step 2: load the Ontology plugin that contains the implementation 
+      File ontoHome = new File(Gate.getPluginsHome(),"Ontology"); 
+      Gate.getCreoleRegister().addDirectory(ontoHome.toURL()); 
+      
+      System.out.println("GATE HOME:\n");
+      System.out.println(Gate.getGateHome());
+
+      // step 3: set the parameters 
+      FeatureMap fm = Factory.newFeatureMap(); 
+      fm.put("rdfXmlURL", "../../../../../../src/main/resources/owl/CL.owl");
+      //fm.put("rdfXmlURL", "../owl/CL.owl");
+      //this is crap - but it seems to be relative to the plug-in not the gate home
+      fm.put("dataDirectoryURL", "../../../../../../src/main/resources/gate/");
+ 
+
+     //fm.put("baseURI", theBaseURI); 
+      //fm.put("mappingsURL", urlOfTheMappingsFile); 
+      // .. any other parameters 
+ 
+      // step 4: finally create an instance of ontology 
+      Ontology ontology = (Ontology) 
+	  Factory.createResource("gate.creole.ontology.impl.sesame.OWLIMOntology", 
+				 fm); 
+ 
+      // retrieving a list of top classes 
+      Set<OClass> topClasses = ontology.getOClasses(true); 
+ 
+      // for all top classes, printing their direct sub classes and print 
+      // their URI or blank node ID in turtle format. 
+      for(OClass c : topClasses) { 
+	  Set<OClass> dcs = c.getSubClasses(OConstants.DIRECT_CLOSURE); 
+	  for(OClass sClass : dcs) { 
+	      System.out.println(sClass.getONodeID().toTurtle()); 
+	      System.out.println(sClass.getLabels());
+	  } 
+      } 
+
+
       pipeline.add(tokeniser);
+      //pipeline.add(ontology);
       pipeline.add(jape);
 
       // create document and corpus
@@ -67,5 +117,9 @@ public class JapeExample  {
 			 doc.getAnnotations().getAllTypes());
       System.out.println("Found annotations of the following types: " +
 			 doc.getAnnotations());
+
+
+    System.exit(0);
   }
+
 }
