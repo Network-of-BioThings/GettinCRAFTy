15594a82bed98b8c19c1933584cb095de172fba0
Kevin Livingston <livingston@dn51dru7.sunet>
2014-04-26 18:27:33 -0600

    continued extended example.

--- src/main/java/GettingCRAFTy/example.java
+++ src/main/java/GettingCRAFTy/example.java
@@ -1,8 +1,13 @@
 package GettingCRAFTy;
+
+import gate.Gate;
+
 public class example {
 
     // method main(): ALWAYS the APPLICATION entry point
-    public static void main (String[] args) {
+    public static void main (String[] args) throws Exception{
 	System.out.println ("Hello World!");
+
+	gate.Gate.init();
     }
 }
